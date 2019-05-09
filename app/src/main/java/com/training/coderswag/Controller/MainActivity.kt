0ef7e72b0a087135561d8400103ed365c131b5bf
Pratish Shetty <shettypratish1893@gmail.com>
2019-05-09 12:17:40 -0400

    #3

--- app/src/main/java/com/training/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/training/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.training.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.training.coderswag.Adapters.CategoryAdapter
 import com.training.coderswag.Model.Category
 import com.training.coderswag.R
 import com.training.coderswag.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
