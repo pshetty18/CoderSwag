e77ad5b73231b5718099287e742951cf7f3d7624
Pratish Shetty <shettypratish1893@gmail.com>
2019-05-09 14:54:07 -0400

    Refactored for recycler view adapter

--- app/src/main/java/com/training/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/training/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.training.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.training.coderswag.Adapters.CategoryAdapter
+import com.training.coderswag.Adapters.CategoryRecycleAdapter
 import com.training.coderswag.Model.Category
 import com.training.coderswag.R
 import com.training.coderswag.Services.DataService
@@ -12,14 +14,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
 //        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
 //            val category = DataService.categories[i]
--- app/src/main/java/com/training/coderswag/Services/DataService.kt
+++ app/src/main/java/com/training/coderswag/Services/DataService.kt
@@ -9,14 +9,6 @@ object DataService {
             Category("SHIRTS", "shirtimage"),
             Category("HOODIES", "hoodieimage"),
             Category("HATS", "hatimage"),
-            Category("DIGITAL", "digitalgoodsimage"),
-            Category("SHIRTS", "shirtimage"),
-            Category("HOODIES", "hoodieimage"),
-            Category("HATS", "hatimage"),
-            Category("DIGITAL", "digitalgoodsimage"),
-            Category("SHIRTS", "shirtimage"),
-            Category("HOODIES", "hoodieimage"),
-            Category("HATS", "hatimage"),
             Category("DIGITAL", "digitalgoodsimage")
     )
     val hats = listOf(
