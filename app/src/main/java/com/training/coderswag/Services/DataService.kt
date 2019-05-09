b13adbb7edec7cdde4598a1b4ee6b678a5179b94
Pratish Shetty <shettypratish1893@gmail.com>
2019-05-09 16:16:20 -0400

    #8

--- app/src/main/java/com/training/coderswag/Services/DataService.kt
+++ app/src/main/java/com/training/coderswag/Services/DataService.kt
@@ -15,6 +15,10 @@ object DataService {
             Product("Devslopes Graphic Beanie", "$10", "hat1"),
             Product("Devslopes Hat Black", "$20", "hat2"),
             Product("Devslopes Hat White", "$18", "hat3"),
+            Product("Devslopes Hat Snapback", "$12", "hat4"),
+            Product("Devslopes Graphic Beanie", "$10", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat White", "$18", "hat3"),
             Product("Devslopes Hat Snapback", "$12", "hat4")
     )
 
@@ -22,10 +26,19 @@ object DataService {
             Product("Devslopes Hoodie Gray", "$30", "hoodie1"),
             Product("Devslopes Hoodie Red", "$35", "hoodie2"),
             Product("Devslopes Gray Hoodie", "$40", "hoodie3"),
+            Product("Devslopes Black Hoodie", "$25", "hoodie4"),
+            Product("Devslopes Hoodie Gray", "$30", "hoodie1"),
+            Product("Devslopes Hoodie Red", "$35", "hoodie2"),
+            Product("Devslopes Gray Hoodie", "$40", "hoodie3"),
             Product("Devslopes Black Hoodie", "$25", "hoodie4")
     )
 
     val shirts = listOf(
+            Product("Devslopes Shirt Black", "$27", "shirt1"),
+            Product("Devslopes Badge Light Gray", "$15", "shirt2"),
+            Product("Devslopes Logo Shirt Red", "$32", "shirt3"),
+            Product("Devslopes Hustle", "$22", "shirt4"),
+            Product("Kickflip Studios", "$20", "shirt5"),
             Product("Devslopes Shirt Black", "$27", "shirt1"),
             Product("Devslopes Badge Light Gray", "$15", "shirt2"),
             Product("Devslopes Logo Shirt Red", "$32", "shirt3"),
@@ -42,7 +55,7 @@ object DataService {
             "HOODIES" -> hoodies
             else -> digitalGood
         }
-        
+
     }
 
 }
